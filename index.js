3e523eba7384c4ae8a3c60e4a1cbefd84432cc19
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 19:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var recipes = {};
  function updateObjectWithKeyAndValue(obj, key, value) {
    return Object.assign({}, obj, {[key]: value});
  }
-function destructivelyUpdatObjectWithKeyAndValue(obj, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(obj, key, value) {
    obj[key] = value;
    return obj;
 }
