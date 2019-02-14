d481667f6a25199cc137200b659150d9443db51e
Mike Doig <mike.j.doig@gmail.com>
2019-02-14 19:08:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ function deleteFromObjectByKey(obj, key) {
   delete newObj [key];
   return newObj;
 }
+function destructivelyDeleteFromObjectByKey(obj, key) {
+  delete obj [key];
+  return obj;
+}
 
 
 
